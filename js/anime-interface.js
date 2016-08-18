1f6b588bd60880e8682ff20fc6a5ea1030db5150
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2016-08-18 16:22:19 -0700

    add bootstrap columns to layout

--- js/anime-interface.js
+++ js/anime-interface.js
@@ -4,32 +4,21 @@ $(function() {
     event.preventDefault();
 
     var input = $('#user-input').val();
+    $('#output').text('');
 
     $.get("http://cdn.animenewsnetwork.com/encyclopedia/api.xml?anime=~" + input,
     output = function(response) {
 
-      // console.log(response.getElementsByTagName("anime")[0]);
-      // console.log(response.getElementsByTagName("anime"));
-
-      var anime = response.getElementsByTagName("anime"); //outputs array of anime
-
+      var anime = response.getElementsByTagName("anime");
       for (var index = 0; index  < anime.length; index++) {
-        $('#output').append("<h3>" + anime[index].getAttribute("name") + "</h3>");
-
-          var info = anime[index].getElementsByTagName("info")[0];
-          var infoType = info.getAttribute("src");
-          $('#output').append("<img src=' " + infoType + "' />");
-
-        }
-
-      // var x = response.getElementsByTagName("anime")[0];
-      // var txt = x.getAttribute("name");
-      // $('#output').text(txt).append(infoType);
-      //
-      // var info = response.getElementsByTagName("info")[0];
-      // var infoType = info.getAttribute("src");
-      //
-      // $('#image').html("<img src=' " + infoType + "' />");
+        var info = anime[index].getElementsByTagName("info")[0];
+        var infoType = info.getAttribute("src");
+        $('#output').append(
+          "<div class='col-xs-4 content'>" +
+          "<div class='header'><span class='header-text'>" + anime[index].getAttribute("name") + "</span></div>" +
+          "<img class='img-responsive' src=' " + infoType + "' />" +
+          "</div>");
+      }
     });
   });
 });
