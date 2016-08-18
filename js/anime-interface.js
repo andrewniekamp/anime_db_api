7b813509fa53137254797f43e9f2c0abdb21a818
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2016-08-18 16:53:19 -0700

    add plot summary and significant styling

--- js/anime-interface.js
+++ js/anime-interface.js
@@ -13,10 +13,21 @@ $(function() {
       for (var index = 0; index  < anime.length; index++) {
         var info = anime[index].getElementsByTagName("info")[0];
         var infoType = info.getAttribute("src");
+        var summary = '';
+        for (var innerIndex = 0; innerIndex < anime[index].getElementsByTagName("info").length; innerIndex++) {
+          if (anime[index].getElementsByTagName("info")[innerIndex].getAttribute("type") === "Plot Summary") {
+            summary = anime[index].getElementsByTagName("info")[innerIndex].textContent;
+          }
+        }
         $('#output').append(
-          "<div class='col-xs-4 content'>" +
-          "<div class='header'><span class='header-text'>" + anime[index].getAttribute("name") + "</span></div>" +
-          "<img class='img-responsive' src=' " + infoType + "' />" +
+          "<div class='row content'>" +
+            "<div class='col-xs-4'>" +
+              "<img class='img-responsive' src=' " + infoType + "' />" +
+            "</div>" +
+            "<div class='col-xs-8'>" +
+              "<div class='header'><span class='header-text'>" + anime[index].getAttribute("name") + "</span></div>" +
+              "<p>" + summary + "</p>" +
+              "</div>" +
           "</div>");
       }
     });
